d85aa6723c2bafb619553862bf314e637bf2b700
valoeghese <valoeghesemods@gmail.com>
2021-02-27 09:35:21 +1300

    initial commit

---
+++ src/main/java/valoeghese/fabricpy/Fabricpy.java
@@ -0,0 +1,17 @@
+package valoeghese.fabricpy;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.util.Random;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Fabricpy implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("Fabricpy");
+
+	@Override
+	public void onInitialize() {
+		// Add init code here!
+	}
+}
