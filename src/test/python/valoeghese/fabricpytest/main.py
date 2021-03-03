4185b77dc2df9aeb949ac52f576f731ae7adb481
valoeghese <valoeghesemods@gmail.com>
2021-03-03 22:05:01 +1300

    This *doesn't* work

--- src/test/python/valoeghese/fabricpytest/main.py
+++ src/test/python/valoeghese/fabricpytest/main.py
@@ -1,3 +1,11 @@
 from java.lang import System
+from net.minecraft.item import Item
+from net.minecraft.util.registry import Registry
 
 System.out.println("Hello, Fabric World!")
+
+item_settings = Item.Settings().maxCount(4)
+
+custom_item = Item(item_settings)
+
+Registry.register(Registry.ITEM, "fabricpy:item", custom_item)
